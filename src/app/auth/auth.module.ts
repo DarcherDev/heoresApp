825cd55b6888990b5994afb97541251bc636e624
DarcherDev <daniel_aristizabal@outlook.com>
2024-04-03 23:01:06 -0500

    html auth
    instalacion angular material,
    creacion de html de login y registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { LayouPageComponent } from './pages/layou-page/layou-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -15,7 +17,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
