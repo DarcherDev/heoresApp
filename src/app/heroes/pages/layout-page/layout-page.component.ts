b817df626b19e204626c57ed51de0410e3679beb
DarcherDev <daniel_aristizabal@outlook.com>
2024-04-03 23:52:29 -0500

    creacion de sidenav
    se documenta codigo de auth
    y se crear sidenav para modulo de heroes

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LayoutPageComponent {
-
+  // Array de objetos que representan los elementos del sidebar
+  public sidebarItems = [
+    { label: 'listado', icon: 'label', url: './list'},
+    { label: 'añadir', icon: 'add', url: './new-hero'},
+    { label: 'Buscar', icon: 'search', url: './search'}
+  ];
 }
