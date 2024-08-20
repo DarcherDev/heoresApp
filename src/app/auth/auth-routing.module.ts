5ce77df26fbd14634140e9f028ba775788e89f21
DarcherDev <daniel_aristizabal@outlook.com>
2024-08-20 15:09:38 -0500

    recuperacion de archivos
    por formateo de pc de torre de perdio avance del modulo de autentificacion
    donde se crear servicio de autentificacion
    interface de usuario
    se corrige nombre de layout (antes le faltaba la t jejejeje)

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayouPageComponent } from './pages/layou-page/layou-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: LayouPageComponent,
+    component: LayoutPageComponent,
     children: [
       { path: 'login', component: LoginPageComponent},
       { path: 'new-account', component: RegisterPageComponent},
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LayouPageComponent } from './pages/layou-page/layou-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
   declarations: [
-    LayouPageComponent,
+    LayoutPageComponent,
     LoginPageComponent,
     RegisterPageComponent
   ],
